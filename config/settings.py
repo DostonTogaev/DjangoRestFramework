72869bd53b6d4b13e4064c67385a9d566299bbb2
DostonTogaev <togayevd37@gmail.com>
2024-08-22 20:45:18 +0500

    signals

--- config/settings.py
+++ config/settings.py
@@ -197,3 +197,25 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "my_cache_table",
+    }
+}
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
--- post/views.py
+++ post/views.py
@@ -5,6 +5,9 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 from post.serializer import PostSerializer
 from post.models import Post
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 # Create your views here.
 
 
@@ -16,7 +19,20 @@ class PostApiView(ListCreateAPIView):
     def get_queryset(self):
         queryset = Post.objects.select_related('author').prefetch_related('tags')
         return queryset 
+    #@method_decorator(cache_page(60 * 2))
+    def get(self, request, *args, **kwargs):
+        cache_key = 'post_list'
+        posts = cache.get(cache_key)
+        if posts is None:
+            posts = Post.objects.all()
+            results = self.paginate_queryset(posts, request, view=self)
 
+            serializer = PostSerializer(results, many=True)
+            cache.set(cache_key, serializer.data, timeout=60*5)
+            return self.get_paginated_response(serializer.data)
+        return Response(posts)
+        
+    
 class PostModelSet(APIView):
     def get(self, request):
         posts = Post.objects.all()
@@ -51,6 +67,27 @@ class PostDetailApiView(APIView):
     def delete(self):
         pass
 
+    def get(self, request, *args, **kwargs):
+        post_id = kwargs.get('pk')
+        cache_key =f'post_detail_{post_id}'
+        post = cache.get(cache_key)
+        if not post:
+            post = Post.objects.get(id=post_id)
+            serializer = PostSerializer(post, many=False)
+            cache.set(cache_key, serializer.data,timeout=60*3)
+            return Response(serializer.data)
+        return Response(post)
+         
+        if not post:
+        # Agar post keshda mavjud bo'lmasa, uni DB dan yoki boshqa joydan oling
+            post = Post.objects.all()  # Misol uchun: barcha postlarni olish
+            cache.set(cache_key, post, timeout=60*15)  # 15 daqiqaga keshga yozish
+    
+        return Response(post)
+    
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    
